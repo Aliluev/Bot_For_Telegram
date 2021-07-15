1a1649cd09bcdd8146fb575e39efd03177636225
Артём Алилуев <tema_aliluev@outlook.com>
2021-07-15 12:41:58 +0300

    v 0.0.1

---
+++ src/main/java/com/example/bot_for_telegram/BotForTelegramApplication.java
@@ -0,0 +1,13 @@
+package com.example.bot_for_telegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotForTelegramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotForTelegramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bot_for_telegram/BotForTelegramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bot_for_telegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotForTelegramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
